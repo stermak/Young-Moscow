6b95a08318853d0c8add7bc04fc5e57171e03405
Saint Ermak <7107798@mail.ru>
2022-12-27 18:46:40 +0300

    0.0.7

--- app/src/main/java/youngdevs/production/youthmoscow/MainFragment.kt
+++ app/src/main/java/youngdevs/production/youthmoscow/MainFragment.kt
@@ -6,19 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
+class MainFragment : Fragment() {
 
-class First : Fragment() {
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        }
-    }
-
-    fun onCreateView(
+    override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.fragment_main, container, false)
     }
+}
