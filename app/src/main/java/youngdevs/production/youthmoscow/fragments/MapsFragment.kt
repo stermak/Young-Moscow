137c411982edf6b4e0e810df9350d26c5f591ce9
Saint Ermak <7107798@mail.ru>
2022-12-28 17:10:11 +0300

    0.0.10
    New Fragments and button's motions

--- app/src/main/java/youngdevs/production/youthmoscow/fragments/MapsFragment.kt
+++ app/src/main/java/youngdevs/production/youthmoscow/fragments/MapsFragment.kt
@@ -1,21 +1,21 @@
 package youngdevs.production.youthmoscow.fragments
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
-import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import youngdevs.production.youthmoscow.R
 
-class MainFragment : Fragment() {
+class MapsFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        return inflater.inflate(R.layout.fragment_maps, container, false)
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -27,19 +27,19 @@ class MainFragment : Fragment() {
         //Buttons Motions
         BtnHome.setOnClickListener { viewCreate: View? ->
             val bundleHome = Bundle()
-            Navigation.findNavController(view).navigate(R.id.action_mainFragment_self, bundleHome)
+            Navigation.findNavController(view).navigate(R.id.action_mapsFragment_to_mainFragment, bundleHome)
         }
         BtnMaps.setOnClickListener { viewCreate: View? ->
             val bundleHome = Bundle()
-            Navigation.findNavController(view).navigate(R.id.action_mainFragment_to_mapsFragment, bundleHome)
+            Navigation.findNavController(view).navigate(R.id.action_mapsFragment_self, bundleHome)
         }
         BtnFavourites.setOnClickListener { viewCreate: View? ->
             val bundleHome = Bundle()
-            Navigation.findNavController(view).navigate(R.id.action_mainFragment_to_favouritesFragment, bundleHome)
+            Navigation.findNavController(view).navigate(R.id.action_mapsFragment_to_favouritesFragment, bundleHome)
         }
         BtnProfile.setOnClickListener { viewCreate: View? ->
             val bundleHome = Bundle()
-            Navigation.findNavController(view).navigate(R.id.action_mainFragment_to_profileFragment, bundleHome)
+            Navigation.findNavController(view).navigate(R.id.action_mapsFragment_to_profileFragment, bundleHome)
         }
     }
 }
