9d6c34312c99dce94a2d5ce13158718c8a5e0e61
Saint Ermak <7107798@mail.ru>
2023-03-19 18:08:04 +0300

    Redesign
    Чуточку задизайнил что-бы хотя бы как-то красиво было,потом ещё будет редизайн

--- app/src/main/java/youngdevs/production/youngmoscow/data/dao/KudaGoApi.kt
+++ app/src/main/java/youngdevs/production/youngmoscow/data/dao/KudaGoApi.kt
@@ -4,8 +4,8 @@ import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
-import youngdevs.production.youngmoscow.data.entities.ApiResponse
 import youngdevs.production.youngmoscow.data.entities.Event
+import youngdevs.production.youngmoscow.data.utilities.ApiResponse
 
 interface KudaGoApi {
     @GET("events/")
@@ -13,7 +13,7 @@ interface KudaGoApi {
         @Query("fields") fields: String = "id,title,description,images",
         @Query("actual_since") actualSince: Long,
         @Query("location") location: String? = null
-    ): retrofit2.Response<ApiResponse>
+    ): Response<ApiResponse>
     @GET("events/{event_id}/")
     suspend fun getEvent(@Path("event_id") eventId: Int): Response<Event>
 }
