825351f83c33356eb44454871dc5eac93597dab7
Saint Ermak <7107798@mail.ru>
2023-03-19 20:08:08 +0300

    Фикс не полного экрана после нажатия на мероприятие

--- app/src/main/java/youngdevs/production/youngmoscow/presentation/event/EventDetailsFragment.kt
+++ app/src/main/java/youngdevs/production/youngmoscow/presentation/event/EventDetailsFragment.kt
@@ -1,10 +1,10 @@
 package youngdevs.production.youngmoscow.presentation.event
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import youngdevs.production.youngmoscow.R
 import youngdevs.production.youngmoscow.data.entities.Event
