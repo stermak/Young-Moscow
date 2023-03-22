2ee025830feb4926d76729995d6b8c88f622e0fc
Saint Ermak <7107798@mail.ru>
2023-03-23 00:34:15 +0300

    FavouriteEventsFragment
    Будем думать теперь в этом направлении

---
+++ app/src/main/java/youngdevs/production/youngmoscow/presentation/ui/fragments/FavouriteEventsFragment.kt
@@ -0,0 +1,11 @@
+package youngdevs.production.youngmoscow.presentation.ui.fragments
+
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+
+@AndroidEntryPoint
+class FavouriteEventsFragment : Fragment() {
+
+}
+
