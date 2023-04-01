81c10db955aea6c0a599615ca16846c262b3adb4
Saint Ermak <7107798@mail.ru>
2023-04-01 19:53:31 +0300

    Исправление ошибок с UI на маленьких устройствах,добавление метки геолокации на картах

--- app/src/main/java/youngdevs/production/youngmoscow/presentation/ui/fragments/MapsFragment.kt
+++ app/src/main/java/youngdevs/production/youngmoscow/presentation/ui/fragments/MapsFragment.kt
@@ -9,11 +9,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.yandex.mapkit.MapKitFactory
 import com.yandex.mapkit.geometry.Point
 import com.yandex.mapkit.map.CameraPosition
+import com.yandex.mapkit.map.IconStyle
 import com.yandex.mapkit.mapview.MapView
+import com.yandex.runtime.image.ImageProvider
 import dagger.hilt.android.AndroidEntryPoint
+import youngdevs.production.youngmoscow.R
 import youngdevs.production.youngmoscow.databinding.FragmentMapsBinding
 import youngdevs.production.youngmoscow.presentation.viewmodel.MapsViewModel
 
@@ -25,6 +29,7 @@ class MapsFragment : Fragment() {
     private var _binding: FragmentMapsBinding? = null
     private val binding get() = _binding!!
 
+
     // View-компонент для отображения карты
     private lateinit var mapView: MapView
 
@@ -33,6 +38,9 @@ class MapsFragment : Fragment() {
 
     // Код запроса на получение разрешения на доступ к местоположению
     private val locationRequestCode = 1000
+    private val START_ZOOM = 14.0f
+    private var isMapZoomedToCurrentLocation = false
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Инициализация View Binding и возвращение корневого View макета фрагмента
@@ -44,6 +52,11 @@ class MapsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         // Инициализация MapView
         mapView = binding.mapView
+
+        val fab: FloatingActionButton = binding.fab
+        fab.setOnClickListener {
+            moveToCurrentLocation()
+        }
 
         if (viewModel.hasLocationPermission()) {
             // Перемещение к текущему местоположению, если разрешение на доступ к местоположению уже получено
@@ -63,6 +76,18 @@ class MapsFragment : Fragment() {
         }
     }
 
+    private fun addPlacemarkToMap(latitude: Double, longitude: Double) {
+        // Создаем метку
+        val placemarkMapObject = mapView.map.mapObjects.addPlacemark(Point(latitude, longitude))
+
+        // Задаем свойства метки
+        placemarkMapObject.setIcon(ImageProvider.fromResource(requireContext(),
+            android.R.drawable.btn_star_big_on
+        ))
+        placemarkMapObject.isVisible = true
+        placemarkMapObject.opacity = 0.8f
+    }
+
     private fun moveToCurrentLocation() {
         val locationManager = requireActivity().getSystemService(Context.LOCATION_SERVICE) as LocationManager
         if (viewModel.hasLocationPermission()) {
@@ -70,19 +95,39 @@ class MapsFragment : Fragment() {
                 val locationProvider = LocationManager.GPS_PROVIDER
                 val lastKnownLocation = locationManager.getLastKnownLocation(locationProvider)
                 if (lastKnownLocation != null) {
-                    // Перемещение карты к текущему местоположению пользователя
-                    mapView.map.move(
-                        CameraPosition(
-                            Point(lastKnownLocation.latitude, lastKnownLocation.longitude),
-                            14.0f, 0.0f, 0.0f
+                    // Получаем текущую позицию камеры
+                    val cameraPosition = mapView.map.cameraPosition
+
+                    // Перемещение карты к текущему местоположению пользователя с сохранением текущего масштаба и положения камеры
+                    if (!isMapZoomedToCurrentLocation) {
+                        // Если карта еще не была приближена к текущей геолокации, то использовать начальный масштаб
+                        mapView.map.move(
+                            CameraPosition(
+                                Point(lastKnownLocation.latitude, lastKnownLocation.longitude),
+                                START_ZOOM, cameraPosition.azimuth, cameraPosition.tilt
+                            )
                         )
-                    )
+
+                        isMapZoomedToCurrentLocation = true // Установить флаг, что карта была приближена к текущей геолокации
+                    } else {
+                        // Если карта уже была приближена к текущей геолокации, то использовать текущий масштаб
+                        mapView.map.move(
+                            CameraPosition(
+                                Point(lastKnownLocation.latitude, lastKnownLocation.longitude),
+                                cameraPosition.zoom, cameraPosition.azimuth, cameraPosition.tilt
+                            )
+                        )
+                    }
+
+                    // Добавляем метку на текущее местоположение пользователя
+                    addPlacemarkToMap(lastKnownLocation.latitude, lastKnownLocation.longitude)
                 }
             } catch (e: SecurityException) {
                 // Обработка исключения SecurityException при доступе к местоположению
             }
         }
     }
+
 
     override fun onStart() {
         super.onStart()
