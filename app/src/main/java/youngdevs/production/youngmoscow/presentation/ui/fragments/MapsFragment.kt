d2c6dfb19a2a93322d2f80d1652aa34660538d25
Dmitriy Komarov <d.komarov2019@gmail.com>
2023-04-16 23:49:06 +0300

    Нашёл плохую методичку( Теперь карты работают как надо
    Карты отображаются,без лишних модулей для Хилта,Ураа

--- app/src/main/java/youngdevs/production/youngmoscow/presentation/ui/fragments/MapsFragment.kt
+++ app/src/main/java/youngdevs/production/youngmoscow/presentation/ui/fragments/MapsFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import dagger.hilt.android.AndroidEntryPoint
 import youngdevs.production.youngmoscow.databinding.FragmentMapsBinding
@@ -14,28 +15,28 @@ import youngdevs.production.youngmoscow.presentation.viewmodel.MapsViewModel
 import javax.inject.Inject
 
 // Фрагмент, отображающий карту и текущее местоположение пользователя
-@AndroidEntryPoint // аннотация для использования Hilt DI
+@AndroidEntryPoint
 class MapsFragment : Fragment() {
 
-    // Поле для привязки View Binding
     private var _binding: FragmentMapsBinding? = null
     private val binding get() = _binding!!
-
-    // ViewModel для работы с данными
     private val viewModel: MapsViewModel by viewModels()
 
-    @Inject
-    lateinit var googleMap: GoogleMap
+    private lateinit var googleMap: GoogleMap
+
+    private val callback = OnMapReadyCallback { googleMap ->
+        this.googleMap = googleMap
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Инициализация View Binding и возвращение корневого View макета фрагмента
         _binding = FragmentMapsBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        // Инициализация MapView
+        binding.mapView.onCreate(savedInstanceState)
+        binding.mapView.getMapAsync(callback)
 
         val fab: FloatingActionButton = binding.fab
         fab.setOnClickListener {
@@ -45,14 +46,36 @@ class MapsFragment : Fragment() {
 
     override fun onStart() {
         super.onStart()
+        binding.mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.mapView.onResume()
+    }
+
+    override fun onPause() {
+        binding.mapView.onPause()
+        super.onPause()
     }
 
     override fun onStop() {
+        binding.mapView.onStop()
         super.onStop()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onDestroy() {
+        binding.mapView.onDestroy()
+        super.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.mapView.onLowMemory()
     }
 }
--- app/src/main/java/youngdevs/production/youngmoscow/presentation/viewmodel/MapsViewModel.kt
+++ app/src/main/java/youngdevs/production/youngmoscow/presentation/viewmodel/MapsViewModel.kt
@@ -1,17 +1,9 @@
 package youngdevs.production.youngmoscow.presentation.viewmodel
 
-import android.app.Application
 import androidx.lifecycle.ViewModel
-import com.google.android.gms.maps.GoogleMap
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 // MapsViewModel - класс ViewModel, который управляет разрешениями на доступ к геолокации
 @HiltViewModel
-class MapsViewModel @Inject constructor(
-    // Внедрение зависимости для доступа к контексту приложения
-    private val applicationContext: Application,
-    private val googleMap: GoogleMap
-) : ViewModel() {
-
-}
+class MapsViewModel @Inject constructor() : ViewModel()
