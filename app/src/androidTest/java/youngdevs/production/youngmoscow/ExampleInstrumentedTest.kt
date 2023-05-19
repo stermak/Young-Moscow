1c460361ed4d7b1c43885d55a88971fe29c50c78
Alexander Ermakov <7107798@mail.ru>
2023-05-19 05:52:29 +0300

    Тест интерфейса ошибочен

--- app/src/androidTest/java/youngdevs/production/youngmoscow/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/youngdevs/production/youngmoscow/ExampleInstrumentedTest.kt
@@ -19,35 +19,5 @@ import java.util.regex.Pattern.matches
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
 
-    @Rule
-    @JvmField
-    val activityRule = ActivityTestRule(MainActivity::class.java)
-
-    @Test
-    fun testButtonVisibility() {
-        onView(withId(R.id.myButton)).check(matches(isDisplayed()))
-    }
-
-    @Test
-    fun testButtonClick() {
-        onView(withId(R.id.myButton)).perform(click())
-        onView(withId(R.id.myTextView)).check(matches(withText("Button Clicked")))
-    }
-
-    @Test
-    fun testEditTextInput() {
-        val inputText = "Hello"
-        onView(withId(R.id.myEditText)).perform(typeText(inputText), closeSoftKeyboard())
-        onView(withId(R.id.myButton)).perform(click())
-        onView(withId(R.id.myTextView)).check(matches(withText(inputText)))
-    }
-
-    @Test
-    fun testRecyclerViewScroll() {
-        onView(withId(R.id.myRecyclerView)).perform(scrollToPosition<RecyclerView.ViewHolder>(10))
-        onView(withText("Item 10")).check(matches(isDisplayed()))
-    }
-
-    // Другие тесты
 
 }
